36cbc6d047fd1b626da076817c09d286ea62151f
simonecnascimento <simonecnascimento@icloud.com>
2023-03-17 15:31:12 -0400

    vascular channel

--- BatchVasculature.m
+++ BatchVasculature.m
@@ -6,7 +6,7 @@ dataSet = 'Vasculature'; %'Afferents'; %  'Neutrophil_Simone'; %'Pollen'; %'Astr
 dataTablePath = 'R:\Levy Lab\2photon\ImagingDatasets.xlsx'; % 'R:\Levy Lab\2photon\ImagingDatasetsSimone2.xlsx'; %'D:\MATLAB\ImagingDatasets.xlsx'; % 'D:\MATLAB\NGCdata.xlsx';  Simone
 dataTable = readcell(dataTablePath, 'sheet',dataSet);  % 'NGC', ''
 colNames = dataTable(1,:); dataTable(1,:) = [];
-dataCol = struct('mouse',find(contains(colNames, 'Mouse')), 'date',find(contains(colNames, 'Date')), 'FOV',find(contains(colNames, 'FOV')), 'vasc',find(contains(colNames, 'vascChan')),...
+dataCol = struct('mouse',find(contains(colNames, 'Mouse')), 'date',find(contains(colNames, 'Date')), 'FOV',find(contains(colNames, 'FOV')), 'vascChan',find(contains(colNames, 'VascChan')),...
     'volume',find(contains(colNames, 'Volume')), 'run',find(contains(colNames, 'Runs')), 'Ztop',find(contains(colNames, 'Zbot')), 'Zbot',find(contains(colNames, 'Ztop')), 'csd',find(contains(colNames, 'CSD')), 'ref',find(contains(colNames, 'Ref')), 'done',find(contains(colNames, 'Done')));
 Nexpt = size(dataTable, 1);
 dataTable(:,dataCol.date) = cellfun(@num2str, dataTable(:,dataCol.date), 'UniformOutput',false);
@@ -22,7 +22,7 @@ Npresent = numel(xPresent);
 for x = xPresent  %30 %x2D % x2Dcsd % x3D %% 51
     % Experiment/run level metadata
     [expt{x}, runInfo{x}, regParams, projParam] = ParseDataTable(dataTable, x, dataCol, dataDir); % , regParams, projParam
-    expt{x}.vascChan = 'red';
+    %expt{x}.vascChan = 'red';
     [Tscan{x}, runInfo{x}] = GetTime(runInfo{x});
 
     % Get locomotion data
